6720855879ae9d9683d6d9cb3ca8f62b58a5a1cc
JCaughley <jamcaughley@gmail.com>
2020-06-18 13:09:46 +1200

    Working Automation and AudioTree etc!
    Baxter n Jamie working with Jon

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -54,18 +54,22 @@ public:
     //==============================================================================
     void getStateInformation (MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
-    float degradeAmount = 8.0;
+    float degradeAmount = 8.0; // range will be from 1.0 to 100,000.0 we will use an exponential slider to make up for the logarithmic behaviour of the algorithm.
     int lastSampleRate;
     void updateFilter();
     void updateParameters();
-    float lowPassFreq = 20000.0;
-    float highPassFreq = 20.0;
+    float lowPassFreq = 100.0;//range will be 20k to 20
+    float highPassFreq = 20.0; //range will be 20 to 20k
+    float mixValue = 1.0f; //range will be 0.0 to 1.0
    
    AudioProcessorValueTreeState tree;
 
 private:
-    dsp::ProcessorDuplicator< dsp::StateVariableFilter::Filter<float>, dsp::StateVariableFilter::Parameters<float>> lowPassFilter;
-    dsp::ProcessorDuplicator< dsp::StateVariableFilter::Filter<float>, dsp::StateVariableFilter::Parameters<float>> hiPassFilter;
-    //==============================================================================
+    std::atomic<float>* quantParam = nullptr;
+    std::atomic<float>* lowPassParam = nullptr;
+   // dsp::ProcessorDuplicator< dsp::StateVariableFilter::Filter<float>, dsp::StateVariableFilter::Parameters<float>> lowPassFilter;
+    //dsp::ProcessorDuplicator< dsp::StateVariableFilter::Filter<float>, dsp::StateVariableFilter::Parameters<float>> hiPassFilter;
+    dsp::StateVariableFilter::Filter<float>  lowPassFilter;
+    dsp::StateVariableFilter::Parameters<float> lowPassParams; //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SummativeAssessmentAudioProcessor)
 };
