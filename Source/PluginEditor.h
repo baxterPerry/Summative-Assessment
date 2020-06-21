0329821d0116676abcfa3f2eae7d9d29b204270d
JCaughley <jamcaughley@gmail.com>
2020-06-21 22:51:33 +1200

    Implementing Baxter's Changes
    Baxter connected sliders to value changes and made it able to store state even after closing session.

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -19,12 +19,16 @@
 class SummativeAssessmentAudioProcessorEditor  : public AudioProcessorEditor
                                                {
 public:
-    SummativeAssessmentAudioProcessorEditor (SummativeAssessmentAudioProcessor&);
+    SummativeAssessmentAudioProcessorEditor (SummativeAssessmentAudioProcessor&, AudioProcessorValueTreeState& );
     ~SummativeAssessmentAudioProcessorEditor();
+    
+    //typedef AudioProcessorValueTreeState::SliderAttachment SliderAttachment;
 
     //==============================================================================
     void paint (Graphics&) override;
     void resized() override;
+                                                   
+                                                                                                
 
 private:
    
@@ -33,6 +37,22 @@ private:
     Slider hiPassSlider;
     Slider mixSlider;
     Slider inputGainSlider;
+    Slider outputGainSlider;
+                                                   
+    Label quantSliderLabel;
+    Label lowPassSliderLabel;
+    Label hiPassSliderLabel;
+    Label mixSliderLabel;
+    Label inputGainSliderLabel;
+    Label outputGainSliderLabel;
+                                       
+    std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> inputGainSliderAttachment;
+    std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> quantSliderAttachment;
+    std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> lowPassSliderAttachment;
+    std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> hiPassSliderAttachment;
+    std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> mixSliderAttachment;
+    std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> outputGainSliderAttachment;
+
     
     LookAndFeel_V4 otherLookAndFeel;
     LookAndFeel_V4 bar1LookAndFeel;
@@ -43,7 +63,11 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SummativeAssessmentAudioProcessor& processor;
+    AudioProcessorValueTreeState& tree;
+                                                   
+                                                   
     public:
     std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> sliderValue;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SummativeAssessmentAudioProcessorEditor)
 };
+
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -74,3 +74,4 @@ private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SummativeAssessmentAudioProcessor)
 };
+
